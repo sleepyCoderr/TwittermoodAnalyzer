c94c332b2a1e40bcf55b797e358978a95df09390
Mofal91 <33989934+Mofal91@users.noreply.github.com>
2019-01-25 11:43:11 -0500

    tweets_db finally working as it should

--- static/app.js
+++ static/app.js
@@ -1,14 +1,22 @@
 
 tweetData=d3.json("../realdonaldtrump.json").then(function(data){
-    return data;
-    });
+    return tweetData;});
+
+// tweetData=d3.json("../realdonaldtrump").then(function(data){
+//     return data;
+//     });
 // sentimentData=d3.json("/sentiment").then(function(data){
 //     return data;
 //     });
     Promise.all([tweetData]).then(function(values){
         data=values[0];
-        console.log(data[2100])
+        console.log(data )});
         // data2=values[1];
-    });
+    
+    // Promise.all([tweetData,]).then(function(values){
+    //     data=values[0];
+    //     console.log(data)
+    // });
 
-
+    
+    
